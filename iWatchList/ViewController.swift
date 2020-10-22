0d8e28382214c6227c95abd6371165552c19b810
Brian Gomes <dev.briangomes@gmail.com>
2020-10-22 19:38:50 +0100

    Initial Commit

---
+++ iWatchList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iWatchList
+//
+//  Created by Brian Gomes on 22/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
